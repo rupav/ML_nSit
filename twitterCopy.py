c56751f7c00a107264c0f0b83d5076ec89a3a032
rupav jain <rupavwinchester@gmail.com>
2017-09-17 17:04:51 +0530

    fetch complete tweets and push them into a set.

--- twitterCopy.py
+++ twitterCopy.py
@@ -20,30 +20,62 @@ api = tweepy.API(auth,wait_on_rate_limit=True)
 
 bag_of_tweets = set()
 tweets = []
+tweets_set = set()
 count = 100
 epoch = 180
-places = api.geo_search(query = "INDIA", granularity = "country")
+places = api.geo_search(query = "India", granularity = "country")
 places_id = places[0].id
 print(places_id)
+trend = "Chelsea"
+#query = 'GET President&place:{}'.format(places_id)
+#"truncated":true
+query = 'GET {}&place:{}'.format(trend,places_id)
+wasted_tweets_count = 0
 
-query = 'GET President&place:{}'.format(places_id)
+'''
+Tweepy return retweeted tweets as truncated!
 
-for i in range(epoch):
+Few new tweets can still be trancated.
+
+Both truncated are seen be '...'
+
+'''
+
+for i in range(8):
 
 	assert epoch<=180
 	
-	public_tweets = api.search(query,lang = 'en',count = count)
+	public_tweets = api.search(query,lang = 'en',count = 20)     #tweet_mode = extended is giving error while truncated = False is not working!
 	
-
+	#print(dir(public_tweets[0]))        # to get all functions of this object!!!
 	
 	for j in range(len(public_tweets)):
-
-		tweets.append(public_tweets[j].text)	
+		#print(public_tweets[j].truncated)
+		#print(public_tweets[j].user.url)
+		print("*********************************")
+		if(public_tweets[j].truncated==False):
+			tweets.append(public_tweets[j].text)	     # to save only complete tweets
+			tweets_set.add(public_tweets[j].text)
+		else:
+			wasted_tweets_count += 1
 		
 
 
-print(len(tweets))
-print(tweets[:5])
+print("Fetched tweets are: ",len(tweets))
+print("Wasted tweets from fetched: ",wasted_tweets_count)
+
+i = 1
+for i in range(len(tweets_set)):
+	print(tweets_set.pop())
+	print("\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/\\/")
+print("final tweets o/p : ",i+1)
+'''
+for tweet in tweets:
+	print(tweet)
+	print("*******")
+'''
+
+#print(tweets[:5])
 '''
 print(len(public_tweets))
 print(type(public_tweets))
@@ -52,11 +84,4 @@ for i in range(len(public_tweets)):
 	print(public_tweets[i].text)
 	print(len(public_tweets[i].text))
 	print("\n------")
-'''
-
-'''
-SentiWordNet can be used.
-WordNet for creation of training Data, then using a classifier to predict!
-it might take time.
-We will add other tweets into existing training dataset!
 '''
